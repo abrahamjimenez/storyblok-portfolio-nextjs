946a733e97a9e19be4a6026772dd22813ee56b58
Abraham Jimenez <64721034+abrahamjimenez@users.noreply.github.com>
2025-06-13 17:52:19 -0600

    Main is storyblok editable now

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { StoryblokServerComponent } from "@storyblok/react/rsc";
+import {storyblokEditable, StoryblokServerComponent} from "@storyblok/react/rsc";
 import type { PageStoryblok } from "@/types/component-types-sb";
 
 const Page = ({ blok }: { blok: PageStoryblok }) => {
   return (
-    <main>
+    <main {...storyblokEditable(blok)}>
       {blok?.body?.map((nestedBlok) => (
         <StoryblokServerComponent blok={nestedBlok} key={nestedBlok?._uid} />
       ))}

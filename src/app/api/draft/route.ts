9c51279c86f9f043fcd3018039220d17bf526f3b
Abraham Jimenez <64721034+abrahamjimenez@users.noreply.github.com>
2025-06-15 23:19:12 -0600

    Enable if in development

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -2,8 +2,14 @@ import { draftMode} from "next/headers";
 import {redirect} from "next/navigation";
 
 export async function GET() {
-    const draft = await draftMode()
-    draft.enable()
+    if (process.env.NEXT_PUBLIC_NODE_ENV === "development") {
+        const draft = await draftMode()
+        draft.enable()
 
+    // return new Response(`Draft mode is enabled`)
+        redirect("/")
+    }
+
+    // return new Response(`Draft mode is not enabled`)
     redirect("/")
 }

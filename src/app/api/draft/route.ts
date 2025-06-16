9874d77f64ccdac9ac88b269fa3605624dce9da3
Abraham Jimenez <64721034+abrahamjimenez@users.noreply.github.com>
2025-06-15 22:43:46 -0600

    Test enabling always

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -1,17 +1,9 @@
 import { draftMode} from "next/headers";
 import {redirect} from "next/navigation";
 
-export async function GET(request: Request) {
-    const {searchParams}= new URL(request.url)
-    const slug = searchParams.get("slug")
-
-    if (!slug) {
-        return new Response("Missing slug parameter", {status: 400})
-    }
-
+export async function GET() {
     const draft = await draftMode()
     draft.enable()
 
-    // return new Response(`Draft mode is enabled, ${searchParams}, and, ${slug}`)
-    redirect(slug)
+    redirect("/")
 }

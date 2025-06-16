f0e811112079062362be1e3ae1eeb2f304697d1f
Abraham Jimenez <64721034+abrahamjimenez@users.noreply.github.com>
2025-06-15 23:27:39 -0600

    Return redirect if in prod or prev

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -1,15 +1,22 @@
 import { draftMode} from "next/headers";
 import {redirect} from "next/navigation";
 
-export async function GET() {
+export async function GET(request: Request) {
+    const {searchParams} = new URL(request.url)
+    const slug = searchParams.get("slug")
+
+    if (!slug) {
+        return redirect("/")
+    }
+
     if (process.env.NEXT_PUBLIC_NODE_ENV === "development") {
         const draft = await draftMode()
         draft.enable()
 
     // return new Response(`Draft mode is enabled`)
-        redirect("/")
+        return redirect(`${slug}?${searchParams.toString()}`)
     }
 
     // return new Response(`Draft mode is not enabled`)
-    redirect("/")
+    return redirect("/")
 }
